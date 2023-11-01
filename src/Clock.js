4237db827405456d7853d97ce149de2993cfce0c
maureen <maureen.chelangat@student.moringaschool.com>
2023-11-01 12:19:45 +0300

    completed assignment

--- src/Clock.js
+++ src/Clock.js
@@ -1,12 +1,19 @@
 import React, { useState, useEffect } from "react";
 
 function Clock() {
+  console.log("component rendering")
   const [time, setTime] = useState(new Date());
 
   useEffect(() => {
-    setInterval(() => {
+    const timerID=
+        setInterval(() => {
       setTime(new Date());
     }, 1000);
+
+    return function cleanup(){
+      console.log('component cleaned')
+      clearInterval(timerID);
+    };
   }, []);
 
   return <div>{time.toString()}</div>;
